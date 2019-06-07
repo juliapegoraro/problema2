54549b8affadfc5ec1cda964b3eb33aa7081adce
juliapegoraro <42074151+juliapegoraro@users.noreply.github.com>
2019-06-07 20:06:39 -0300

    Add files via upload

---
+++ ParalelaJulia/app/src/main/java/com/example/paralelajulia/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.paralelajulia;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void chama(View view) {
+        alertDialog.setMessage("promocao");
+        alertDialog.show();
+    }
+
+}
